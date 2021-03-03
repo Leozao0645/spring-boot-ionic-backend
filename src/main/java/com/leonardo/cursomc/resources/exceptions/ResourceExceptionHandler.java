3b9405bfa95f56ad018202d699be321ca66adb52
Leonardo de Souza <leonardodesouza0645@gmail.com>
2021-03-02 22:38:40 -0300

    Ajustes no Endpoint /categorias/{id}

---
+++ src/main/java/com/leonardo/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.leonardo.cursomc.resources.exceptions;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.leonardo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(Calendar.getInstance().getTime()));
+		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+	}
+	
+}
