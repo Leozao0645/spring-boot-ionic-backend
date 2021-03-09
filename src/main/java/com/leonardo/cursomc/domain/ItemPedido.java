e506483f8ed58e75b37e56ffef18925ff80fcbfe
Leonardo de Souza <leonardodesouza0645@gmail.com>
2021-03-09 20:28:45 -0300

    Endpoint/pedidos/{id} disponibilizado

--- src/main/java/com/leonardo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/leonardo/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -53,7 +56,8 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
