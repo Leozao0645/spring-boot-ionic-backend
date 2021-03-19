fe42ea74001342bdbf6b0bf94a4262719f7dbb7e
Leonardo de Souza <leonardodesouza0645@gmail.com>
2021-03-18 23:31:08 -0300

    Validação sintática com Bean validation

--- src/main/java/com/leonardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leonardo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leonardo.cursomc.domain.Categoria;
+import com.leonardo.cursomc.dto.CategoriaDTO;
 import com.leonardo.cursomc.repositories.CategoriaRepository;
 import com.leonardo.cursomc.services.exceptions.DataIntegrityException;
 import com.leonardo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria findDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
